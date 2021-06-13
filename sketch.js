7ad843f89ad83ca5c78eb9fed5a8f362221dc345
iram-afsha <85054305+iram-afsha@users.noreply.github.com>
2021-06-13 18:21:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+var friends = ['sam', 'john', 'ramy'];
+
+function setup() {
+  
+
+   c
+  console.log("Hi ",friends[2],"!Please grace us with your presence at my birthday celebration");
+}
+
+
+function draw() {
+  background(220);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+console.log(friends);
+  x
+*/
+
+
+
